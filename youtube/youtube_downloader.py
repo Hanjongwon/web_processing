b9d0c3e4973e3921d0ab20159eab36105f5ab1d3
Hanjongwon <herowhddnjs@naver.com>
2021-08-16 22:24:47 +0900

    commit success

--- youtube/youtube_downloader.py
+++ youtube/youtube_downloader.py
@@ -1,3 +1,14 @@
 import pytube
 
-yt = pytube.YouTube("") #다운 받을 동영상 URL 저장
+yt = pytube.YouTube("https://www.youtube.com/watch?v=wkVlb8rSies") #다운 받을 동영상 URL 저장
+videos = yt.streams
+
+#print("videos", videos)
+
+for i in range(len(videos)):
+    print(i, ', ', videos[i])
+
+cNum = int(input("다운 받을 화질은?"))
+down_dir = "C:/Users/user/PycharmProjects/web_processing/youtube"
+
+videos[cNum].download(down_dir)
